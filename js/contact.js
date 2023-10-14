2a950687c73717ef80bc36d83a01ec4dc02ea48a
trevin-livele <liveletrevin6@gmail.com>
2023-10-14 11:26:14 +0300

    added the new files

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // You can add your own code here to handle the form submission
+    // For example, send the form data to a server using AJAX
+  });
+  
